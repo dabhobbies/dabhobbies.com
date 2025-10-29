4a6e3aee014cbaf67b6bc4d4b3c906c570458c98
Dab Hobbies <dabhobbies.webmaster@gmail.com>
2025-10-29 15:13:49 +0000

    Tolong kamu install sanity studio di /studio
    Dengan projectid ko6ixh1i

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ko6ixh1i',
+    dataset: 'dab-g7xk2p9r1mqd_data'
+  }
+})
---
+++ studio/schema/product.ts
@@ -0,0 +1,117 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'brand',
+      title: 'Brand',
+      type: 'string',
+    },
+    {
+        name: 'category',
+        title: 'Category',
+        type: 'string',
+        options: {
+            list: ['Helmets', 'Jackets', 'Gloves', 'Boots', 'Pants', 'Suits'],
+            layout: 'radio'
+        }
+    },
+    {
+        name: 'description',
+        title: 'Description',
+        type: 'text'
+    },
+    {
+        name: 'longDescription',
+        title: 'Long Description',
+        type: 'text'
+    },
+    {
+        name: 'price',
+        title: 'Price',
+        type: 'number'
+    },
+    {
+        name: 'rating',
+        title: 'Rating',
+        type: 'number',
+    },
+    {
+        name: 'reviewCount',
+        title: 'Review Count',
+        type: 'number'
+    },
+    {
+        name: 'sizes',
+        title: 'Sizes',
+        type: 'array',
+        of: [{type: 'string'}]
+    },
+    {
+        name: 'colors',
+        title: 'Colors',
+        type: 'array',
+        of: [{type: 'string'}]
+    },
+    {
+        name: 'gender',
+        title: 'Gender',
+        type: 'string',
+        options: {
+            list: ['Unisex', 'Men', 'Women'],
+            layout: 'radio'
+        }
+    },
+    {
+        name: 'materials',
+        title: 'Materials',
+        type: 'array',
+        of: [{type: 'string'}]
+    },
+    {
+        name: 'protection',
+        title: 'Protection',
+        type: 'array',
+        of: [{type: 'string'}]
+    },
+    {
+        name: 'certification',
+        title: 'Certification',
+        type: 'string'
+    },
+    {
+        name: 'specialFeatures',
+        title: 'Special Features',
+        type: 'array',
+        of: [{type: 'string'}]
+    },
+    {
+        name: 'weight',
+        title: 'Weight (kg)',
+        type: 'number'
+    }
+  ],
+}
