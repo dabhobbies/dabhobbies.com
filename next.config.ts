b07e5f792ce274ec3c89898a31d3dd5b38ac71de
Erdy Yudanto <erdy.yudanto@gmail.com>
2026-01-23 13:42:26 +0700

    Fix build: Force webpack build and remove deprecated eslint config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 import withPWAInit from '@ducanh2912/next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
